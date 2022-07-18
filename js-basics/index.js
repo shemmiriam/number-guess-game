3e2a70c293018f3f57e6ab4ea3093566557c74f6
shem miriam <shemmiriam93@gmail.com>
2022-07-18 14:59:30 +0300

    Add files via upload
    beginner

---
+++ js-basics/index.js
@@ -0,0 +1,62 @@
+// This is my first javascript code!
+/*console.log('Hello world');
+
+document.write('hello')
+
+
+
+const values =['a', 'b','c'];
+console.log(Array.isArray (values));
+
+console.log(values[0])
+console.log(values[1])
+console.log(values[2])
+console.log(values[3])
+
+values.push('d', 'e', 'f')
+console.log(values)
+
+const last=values.pop();
+console.log(values, last);
+console.log(values.indexOf('a'));
+
+const set=values.filter(function(item){
+    console.log('item: ', item);
+    return item >'b';
+})
+console.log(set)
+
+let x = 10;
+if (x == 10) {
+    let x = 20;
+    console.log(x); // 20:  reference x inside the block
+}
+console.log(x);
+
+var a = 10;
+console.log(window.a); // 10
+
+for (var i = 0; i < 5; i++) {
+    setTimeout(function () {
+        console.log(i);
+    }, 1000);
+}
+
+for (var i = 0; i < 5; i++) {
+    (function (j) {
+        setTimeout(function () {
+            console.log(j);
+        }, 1000);
+    })(i);
+}
+{ // enter new scope, TDZ starts
+    let log = function () {
+        console.log(message); // messagedeclared later
+    };
+
+    // This is the TDZ and accessing log
+    // would cause a ReferenceError
+
+    let message= 'Hello'; // TDZ ends
+    log(); // called outside TDZ
+}*/
